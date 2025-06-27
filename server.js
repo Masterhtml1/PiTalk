87ed3de0012d0fbc742f83c75675333862a491dc
elgooghtml <126972084+Masterhtml1@users.noreply.github.com>
2025-06-27 17:44:07 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const PORT = 3000;
+http.listen(PORT, () => {
+  console.log(`PiTalk listening on port ${PORT}`);
+});
